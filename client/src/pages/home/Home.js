d1a971e097e7fb8f4c9224bb749a7b851407e699
jordanostin <jordan.ostin@outlook.fr>
2024-07-26 14:16:16 +0200

    update 26/07

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,10 +1,18 @@
 import './home.scss'
+import {Header} from '../../components/header/header'
 
 export const Home = () => {
 
     return(
-        <>
-            <h1>home</h1>
+        <> 
+            <Header/>
+            <div className='cadre'>
+                <h2>Notre forge virtuelle</h2>
+
+                <h3>Au service de votre imagination</h3>
+
+                <p>Chez Blackpearl Factory, nous sommes guidés par la conviction que chaque élan créatif mérite de prendre forme.</p>
+            </div>
         </>
     );
 }
--- client/src/routes/route.js
+++ client/src/routes/route.js
@@ -1,11 +1,10 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {Header} from "../components/header/header";
 import {Home} from "../pages/home/Home";
 
 export const Navigation = () => {
+
     return(
         <BrowserRouter>
-            <Header/>
             <Routes>
                 <Route path='/' element={<Home />}/>
             </Routes>
