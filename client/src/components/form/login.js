409aba63f17911a4a789a3e95d85615322176fc3
jordanostin <jordan.ostin@outlook.fr>
2024-07-29 10:48:56 +0200

    update 29/07

---
+++ client/src/components/form/login.js
@@ -0,0 +1,72 @@
+import {useDispatch} from 'react-redux'
+import {addUser} from '../../store/slices/user/userSlice'
+import {useNavigate} from 'react-router-dom'
+
+export const login = () =>{
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) =>{
+
+        e.preventDefault();
+
+        const user = new FormData(e.target);
+
+        fetch(`${pocess.env.REACT_APP_API_URL}/auth/login`,{
+
+            method: 'POST',
+
+            body: JSON.stringify({
+                email: user.get('email'),
+                password: user.get('password')
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+
+            const name = data.user.name
+            const email = data.user.email
+            const password = data.user.password
+            const isAdmin = data.user.isAdmin
+            const jwt = data.jwt
+
+            localStorage.setItem('token', data.token);
+
+            dispatch(addUser({name,email,password,isAdmin,jwt}))
+        })
+        .catch((err) => console.log(err))
+
+        //navigate('/')
+    }
+
+    return(
+        <>
+            <form onSubmit={handleSubmit} className='box'>
+                <div className='container'>
+
+                    <div className='top'>
+                        <h2>Login</h2>
+                    </div>
+
+                    <div className='input-field'>
+                        <input type='text' id='email' name='email' placeholder='email' className='input'/>
+                    </div>
+
+                    <div className='input-field'>
+                        <input type='password' id='password' name='password' placeholder='password' className='input'/>
+                    </div>
+
+                    <div className='input-field'>
+                        <input type="Submit" defaultValue='Login' className='submit'/>
+                    </div>
+
+                </div>
+            </form>
+        </>
+    );
+}
---
+++ client/src/components/form/register.js
@@ -0,0 +1,77 @@
+import {useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import {addUser} from '../../store/slices/user/userSlice';
+
+export const Register =()=>{
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleSubmite = (e) =>{
+
+        e.preventDefault();
+
+        const newUser = new FormData(e.target);
+
+        fetch(`${process.env.REACT_APP_API_URL}/auth/register`,{
+            method: 'POST',
+            body: JSON.stringify({
+                name: newUser.get('name'),
+                email: newUser.get('email'),
+                password: newUser.get('password')
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            const name = data.user.name
+            const email = data.user.email
+            const password = data.user.password
+            const isAdmin = data.user.isAdmin
+            const jwt = data.jwt
+
+            localStorage.setItem('jwt', data.token);
+
+            dispatch(addUser({name,email,password,isAdmin,jwt}))
+
+        })
+        .catch((err) => console.log(err));
+
+        //navigate('/login');
+
+    }
+
+    return(
+        <>
+            <form onSubmit={handleSubmit} className='box'>
+                <div className='container'>
+
+                    <div className='top'>
+                        <h2>Register</h2>
+                    </div>
+
+                    <div className='input-field'>
+                        <input type='text' id='name' name='name' placeholder='Name' className='input'/>
+                    </div>
+
+                    <div className='input-field'>
+                        <input type='text' id='email' name='email' placeholder='Email' className='input'/>
+                    </div>
+
+                    <div className='input-field'>
+                        <input type='password' id='password' name='password' placeholder='Password' className='input'/>
+                    </div>
+
+                    <div className='input-field'>
+                        <input type="Submit" defaultValue='Register' className='submit'/>
+                    </div>
+                </div>
+            </form>
+        </>
+    )
+
+}
