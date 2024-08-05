5ad662bdda74b3b02f50143687fee7194da313cf
jordanostin <jordan.ostin@outlook.fr>
2024-08-05 11:54:31 +0200

    update 05/08

--- client/src/components/article/article.js
+++ client/src/components/article/article.js
@@ -3,19 +3,27 @@ import {useEffect, useState} from 'react';
 
 export const Articles = () => {
 
-    const [artticles ,setArticles] = useState([]);
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+    const [articles ,setArticles] = useState([]);
 
-    fetch(`${process.env.REACT_APP_API_URL}/articles`)
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((err) => console.log(err))
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}/articles`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data.articles);
+
+            setArticles(data.articles);
+        })
+        .catch((err) => console.log(err))
+    },[])
 
     return (
         <>
+            {articles.map((article) => (
+                <div key={article._id} className='article'>
+                    <h2>{article.title}</h2>
+                    <p>{article.description}</p>
+                </div>
+            ))}
         </>
     )
 
